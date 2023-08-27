22c84ef99a7eb2f7e78a3c605ac04f0e8c83c44a
jjokah <johnjohnsonokah@gmail.com>
2023-08-27 14:10:40 +0100

    Build the course models

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Subject, Course, Module
 
-# Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    """Register the Subject Model to admin site."""
+
+    list_display = ["title", "slug"]
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class ModuleInLine(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    """Register the Course Model to admin site."""
+
+    list_display = ["title", "subject", "created"]
+    list_filter = ["created", "subject"]
+    search_fields = ["title", "overview"]
+    prepopulated_fields = {"slug": ("title",)}
+    inlines = [ModuleInLine]
