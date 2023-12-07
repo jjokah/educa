4fd66593c89974318201ef1eaeca3587b4e42f35
jjokah <johnjohnsonokah@gmail.com>
2023-12-07 09:10:40 +0100

    Add "kolo" debugging tool

--- educa/settings.py
+++ educa/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "kolo.middleware.KoloMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
