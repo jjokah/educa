98dbfaca02148d0a8fb9a840787bbfed8e0a1257
jjokah <johnjohnsonokah@gmail.com>
2024-08-05 05:24:31 +0100

    Create Published model manager

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,13 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -24,6 +31,8 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT
     )
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
