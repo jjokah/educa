b92fd07fc8fad3d1deab7dda426defc7a6dc77cb
jjokah <johnjohnsonokah@gmail.com>
2024-08-07 22:22:24 +0100

    Use canonical URLs for models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -43,3 +44,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[self.id]
+        )
