126fb543b6cd5d2afc095d5d91e51e35b6ca25cf
jjokah <johnjohnsonokah@gmail.com>
2024-08-07 21:42:43 +0100

    Build list and detail views

--- educa/urls.py
+++ educa/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("admin/", admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog')),
     path('course/', include('courses.urls')),
 ]
 if settings.DEBUG:
