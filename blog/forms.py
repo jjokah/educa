91b77c84d44a1985e3fd80258cb7c963d7d970e3
jjokah <johnjohnsonokah@gmail.com>
2024-08-08 20:21:41 +0100

    Create a comment system

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 
 
 class EmailPostForm(forms.Form):
@@ -9,4 +10,8 @@ class EmailPostForm(forms.Form):
         required=False,
         widget=forms.Textarea
     )
-    
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
