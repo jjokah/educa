7bfee27a3f17ffe63ae50127de3998bb55b4d623
jjokah <johnjohnsonokah@gmail.com>
2024-08-08 09:45:17 +0100

    Build case-based view for post list

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,18 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from .models import Post
+
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
