c882a783dfa669d025e7a68d1ddc5bbe6a0724f5
jjokah <johnjohnsonokah@gmail.com>
2024-08-11 01:56:14 +0100

    Create feeds for blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 
 app_name = 'blog'
@@ -19,4 +20,5 @@ urlpatterns = [
     path(
         '<int:post_id>/comment/', views.post_comment, name='post_comment'
     ),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
