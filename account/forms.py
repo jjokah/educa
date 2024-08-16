2b5c1023070056b925dbc38a7249704733ffb861
jjokah <johnjohnsonokah@gmail.com>
2024-08-16 02:34:41 +0100

    Build a custom authentication backend

--- account/forms.py
+++ account/forms.py
@@ -3,6 +3,9 @@ from django.contrib.auth import get_user_model
 from .models import Profile
 
 
+User = get_user_model()
+
+
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
@@ -19,7 +22,7 @@ class UserRegistrationForm(forms.ModelForm):
     )
 
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['username', 'first_name', 'email']
 
     def clean_password2(self):
@@ -27,12 +30,29 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError("Passwords don't match.")
         return cd['password2']
+    
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
 
 
 class UserEditForm(forms.ModelForm):
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['first_name', 'last_name', 'email']
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(
+            id=self.instance.id
+        ).filter(
+            email=data
+        )
+        if qs.exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
 
 
 class ProfileEditForm(forms.ModelForm):
--- educa/settings.py
+++ educa/settings.py
@@ -160,3 +160,8 @@ DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
