3d0bfd37c6a23040fbf7ca84d5c793c12f6a482d
jjokah <johnjohnsonokah@gmail.com>
2024-11-27 12:57:23 +0100

    Export orders to CSV files

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,45 @@
+import csv
+import datetime
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import Order, OrderItem
+
+
+def export_to_csv(modeladmin, request, queryset):
+    """
+    Export selected model records to CSV file.
+    """
+    # Get model metadata
+    opts = modeladmin.model._meta
+    # Set filename using model's verbose name
+    content_dispostion = (
+        f'attachment; filename={opts.verbose_name}.csv'
+    )
+    # Configure response as CSV download
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_dispostion
+    writer = csv.writer(response)
+
+    # Get all direct fields (excluding many-to-many and one-to-many) 
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+export_to_csv.short_description = 'Export to CSV'
 
 
 class OrderItemInlne(admin.TabularInline):
@@ -34,3 +74,5 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ['paid', 'created', 'updated']
     # Include OrderItem inline admin interface
     inlines = [OrderItemInlne]
+    # Additional actions to be performed on model list displayed
+    actions = [export_to_csv]
