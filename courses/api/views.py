2a0c0e2d8e668a590bcbf44659d5b4a65d87a5a6
jjokah <johnjohnsonokah@gmail.com>
2025-02-12 19:59:52 +0100

    Create ViewSets and routers

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,25 +1,36 @@
 from django.db.models import Count
 
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 from courses.api.pagination import StandardPagination
-from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
+from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.models import Course, Subject
 
 
-class SubjectListView(generics.ListAPIView):
-    """
-    API view to retrieve a list of all subjects.
-    """
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+    pagination_class = StandardPagination
+
+
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
     pagination_class = StandardPagination
 
 
-class SubjectDetailView(generics.RetrieveAPIView):
-    """
-    API view to retrieve details of a specific subject.
-    """
-    queryset = Subject.objects.annotate(total_courses=Count('courses'))
-    serializer_class = SubjectSerializer
+# class SubjectListView(generics.ListAPIView):
+#     """
+#     API view to retrieve a list of all subjects.
+#     """
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
+#     pagination_class = StandardPagination
 
+
+# class SubjectDetailView(generics.RetrieveAPIView):
+#     """
+#     API view to retrieve details of a specific subject.
+#     """
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
