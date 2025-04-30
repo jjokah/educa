8a9cc489a071c0c2110de33aed0b795fc5c3e2a7
jjokah <johnjohnsonokah@gmail.com>
2025-04-30 17:29:41 +0100

    Create a custom middleware

--- educa/settings/base.py
+++ educa/settings/base.py
@@ -84,6 +84,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'courses.middleware.subdomain_course_middleware',
 ]
 
 
